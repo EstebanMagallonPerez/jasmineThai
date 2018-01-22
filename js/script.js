cfdca39a8a1cbdf2930beab96ae4b3c5ea3e7810
estebanmagallonperez <eperez58>
2018-01-21 20:50:10 -0800

    add loadTop to load in the top items which are marked by the json property top:true

--- js/script.js
+++ js/script.js
@@ -1,6 +1,56 @@
 var menu =
 	{
 		items: {},
+		loadTop:function (menuType, elementID)
+		{
+			var menuList={lunch:"lunch.json","dinner":"dinner.json","gross":"vegetarian.json"};
+			var xhttp = new XMLHttpRequest();
+			var scope = this;
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					menuItems = JSON.parse(xhttp.responseText);
+					scope.items = menuItems;
+					var populatedMenu = '<div class="" style="overflow: hidden;padding-top:8px;padding-bottom:8px;">';
+					populatedMenu += "<div class='category'><h4>Popular Items</h4>"
+					for(var category in menuItems)
+					{
+						var dishes = menuItems[category].items;
+						for(var dish in dishes)
+						{
+							console.log(dishes[dish]);
+							if(dishes[dish].top)
+							{
+								populatedMenu += "<div class='menu-item container'><div class='top'>";
+								populatedMenu += "<span>"+dishes[dish].name+"</span>";
+								if(dishes[dish].price)
+								{
+									populatedMenu += "<span class='price'>$"+dishes[dish].price+"</span>";
+								}
+								populatedMenu += "</div>";
+								if(dishes[dish].description)
+								{
+									populatedMenu += "<div class='description'><span>"+dishes[dish].description+"</span></div>";
+								}
+								if(dishes[dish].list)
+								{
+									populatedMenu += scope.makeList(dishes[dish].list);
+								}
+								if(dishes[dish].table)
+								{
+									populatedMenu += scope.makeTable(dishes[dish].table);
+								}
+								populatedMenu += "</div>";
+							}
+
+						}
+					}
+					populatedMenu += "</div>";
+					document.getElementById(elementID).innerHTML = populatedMenu;
+				}
+			};
+			xhttp.open("GET", "menus/"+menuList[menuType], true);
+			xhttp.send();
+		},
 		load: function (menuType, elementID)
 		{
 			var menuList={lunch:"lunch.json","dinner":"dinner.json","gross":"vegetarian.json"};
