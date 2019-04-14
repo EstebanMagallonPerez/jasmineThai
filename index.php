736cf40ac13f8e62cc6a0ea233c6b952bad30adb
estebanmagallonperez <eperez58>
2019-04-14 16:29:27 -0700

    show sushi tab

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
 		<div class="row">
 			<div class="col s12">
 				<ul class="tabs">
-					<li class="tab col s4"><a href="#lunch">Lunch</a></li>
-					<li class="tab col s4"><a class="active" href="#dinner">Dinner</a></li>
-					<li class="tab col s4"><a href="#vegetarian">Vegetarian</a></li>
-					<li class="tab col s4"><a href="#sushi">Sushi</a></li>
+					<li class="tab col s3"><a href="#lunch">Lunch</a></li>
+					<li class="tab col s3"><a class="active" href="#dinner">Dinner</a></li>
+					<li class="tab col s3"><a href="#vegetarian">Vegetarian</a></li>
+					<li class="tab col s3"><a href="#sushi">Sushi</a></li>
 				</ul>
 			</div>
 			<div id="lunch" class="col s12">Lunch</div>
